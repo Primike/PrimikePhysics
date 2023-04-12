a87580ef4a66f60e5d3e6e5e432b4ff78fba9c47
Prince Avecillas <avecillasprince@yahoo.com>
2023-04-11 21:03:43 -0400

    fixed courses

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -17,37 +17,13 @@ function Courses({courses}) {
         )
     })
     return (
-        <div className='page-wrapper'>
+        <div>
             <Navbar/>
-            <div className="container">
+            <div className="courses-container">
                 {coursesList2}
             </div>
-
-            {/* <div className ="container-xl" >
-                <div className="row row-cols-1">
-                    {courseList}
-                </div>
-            </div> */}
         </div>
     )
 }
 
 export default Courses
-
-
-    // let courseList = courses.map((course, index) => {
-    //     return (
-    //         <div className="col-md-2" key={course.subject}>
-    //             <div className='card'>
-    //                 <img className='card-img-top' src={course.subject_image} id="course-image"/>
-    //                 <div className='card-body'>
-    //                     <h5 id="course-name">{course.subject}</h5>
-    //                     <p id="course-description">Some quick example text to build on the card title and make up the</p>
-    //                 </div>
-    //                 <div className='card-footer p-3 h-100'>
-    //                     <a className="btn btn-primary" href={`/${course.subject}`}>View Course</a>
-    //                 </div>
-    //             </div>
-    //         </div>   
-    //     )
-    // })
