a50e078dbdc10c25c6d2f0d148d56b9ba144c47b
Prince Avecillas <avecillasprince@yahoo.com>
2023-04-12 11:09:06 -0400

    Added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   const tabNames = ['Home', 'Courses', 'Math', 'Grapher'];
   const hrefLinks = [
-    '/PrimikePhysics',
-    '/PrimikePhysics/courses',
-    '/PrimikePhysics/numericalmethods',
-    '/PrimikePhysics/grapher',
+    '/',
+    '/courses',
+    '/numericalmethods',
+    '/grapher',
   ];
 
-  const list = tabNames.map((name, index) => (
-    <div className="navCol" key={name}>
-      <a className="navbarLink" href={hrefLinks[index]}>
+  const list = tabNames.map((name, index) => {
+    return (
+      <a className='navbarLink' key={name} href={hrefLinks[index]}>
         {name}
       </a>
-    </div>
-  ));
+    );
+  });
+
+  const mobileList = tabNames.map((name, index) => {
+    return (
+      <a
+        className='mobileNavbarLink'
+        key={name}
+        href={hrefLinks[index]}
+        onClick={() => setMobileMenuOpen(false)}
+      >
+        {name}
+      </a>
+    );
+  });
 
   return (
     <div className="navbarDiv">
-      <div className="navbarContainer">
-        <div className="navRow">
-          <div className="navColMd3">
-            <a href="/" className="navbarTitle">
-              🔬 PrimikePhysics
-            </a>
-          </div>
-          <div className="navColMd3">
-            <div className="navRow">{list}</div>
-          </div>
-        </div>
+      <div className='navbarContainer'>
+        <a href='/' className='navbarTitle'>
+          🔬 PrimikePhysics
+        </a>
+        <div className='navLinks'>{list}</div>
+        <button className="mobileMenuButton" onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+          ☰
+        </button>
       </div>
+      {mobileMenuOpen && (
+        <div className="mobileMenu">
+          <button className="mobileMenuClose" onClick={() => setMobileMenuOpen(false)}>
+            &times;
+          </button>
+          <div className="mobileMenuContent">{mobileList}</div>
+        </div>
+      )}
       <div className="customContainer">
         <h1 className="homeTitle">Welcome to Primike Physics</h1>
         <h2 className="homeText">Learn physics, math and discover cool stuff!</h2>
@@ -44,3 +64,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
