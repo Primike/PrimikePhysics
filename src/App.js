d022685943cc1c6f886ebe920926cc234c61e4f8
Prince Avecillas <avecillasprince@yahoo.com>
2023-04-11 20:11:22 -0400

    Testing

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home'
-import Courses from './components/Courses/Courses'
-import Course from './components/Courses/components/Course'
+import Home from './components/Home/Home';
+import Courses from './components/Courses/Courses';
+import Course from './components/Courses/components/Course';
 import Grapher from './components/Grapher/Grapher';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import useQuery from './query/useQuery';
 import { useEffect, useState } from "react";
 
 
 function App() {
-  const [courses, setCourses] = useState([])
+  const [courses, setCourses] = useState([]);
 
-  // const { data: courses } = useQuery('https://primike.github.io/data/primikephysics.json')
-  
   useEffect(() => {
     fetch('https://primike.github.io/data/primikephysics.json')
-    .then(res => res.json())
-    .then(courses => setCourses(courses))
-  }, [])
+      .then(res => res.json())
+      .then(courses => setCourses(courses))
+  }, []);
   
   return (
     <div className="App">
-      <HashRouter>
+      <HashRouter basename="/primike">
         <Routes>
-          <Route path="/PrimikePhysics" element={<Home/>} />
-          <Route path="/PrimikePhysics/courses" element={<Courses courses={courses}/>} />
+          <Route path="/PrimikePhysics" element={<Home />} />
+          <Route path="/PrimikePhysics/courses" element={<Courses courses={courses} />} />
           <Route path="/PrimikePhysics/numericalmethods" element={<Home />} />
           <Route path="/PrimikePhysics/grapher" element={<Grapher />} />
           {courses.map((course) => (
-              <Route 
-                path = {`/${course.subject}`} 
-                key ={`${course.subject}course`} 
-                element={<Course course={course}/>}/>
+            <Route 
+              path={`/${course.subject}`} 
+              key={`${course.subject}course`} 
+              element={<Course course={course} />}
+            />
           ))}
+          <Route path="*" element={<Home />} />
         </Routes>
       </HashRouter>
     </div>
